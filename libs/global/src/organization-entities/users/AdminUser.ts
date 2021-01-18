fad659c0a85a89fefcd3a24d3d99a59085921a5f
Zdravko <zdravkokirilov@yahoo.com>
2021-01-18 11:06:12 +0200

    wip

--- libs/global/src/organization-entities/users/AdminUser.ts
+++ libs/global/src/organization-entities/users/AdminUser.ts
@@ -1,1 +1,20 @@
-export class AdminUser {}
+import { IsEmail, IsUUID } from 'class-validator';
+
+import { Email, Tagged, UUIDv4 } from '../../types';
+
+type AdminUserId = Tagged<'AdminUserId', UUIDv4>;
+
+abstract class CommonFields {
+  @IsEmail()
+  email: Email;
+}
+
+export class AdminUser extends CommonFields {
+  @IsUUID()
+  id: AdminUserId;
+}
+
+export class FullAdminUser extends AdminUser {
+  activationCode: string;
+  actionCodeExpiration: Date;
+}
