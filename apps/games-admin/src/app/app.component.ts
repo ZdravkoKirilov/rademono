94b9f38be728cc245ee1a041e4ab496ff4c471e6
Zdravko <zdravkokirilov@yahoo.com>
2021-03-17 10:46:15 +0200

    add ui lib boilerplate

--- apps/games-admin/src/app/app.component.ts
+++ apps/games-admin/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { gosho } from '@end/ui';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   providers: [],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = gosho;
+}
---
+++ libs/ui/projects/main/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of main
+ */
+
+export const gosho = 'vie ste vampir';
