a07a53952e6e286a80491c59826e070eb874fad7
zdravkokirilov <zdravkovasilchevkirilov@gmail.com>
2021-06-21 10:50:32 +0300

    assets wip

--- libs/global/src/organization-entities/Asset.ts
+++ libs/global/src/organization-entities/Asset.ts
@@ -2,11 +2,17 @@ import { Expose } from 'class-transformer';
 import {
   IsIn,
   IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
   IsUUID,
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { switchMap } from 'rxjs/operators';
+import * as e from 'fp-ts/Either';
 
+import { parseAndValidateUnknown, toLeftObs } from '../parsers';
 import { StringOfLength, Tagged, Url, UUIDv4 } from '../types';
 import { OrganizationId } from './Organization';
 
@@ -20,27 +26,79 @@ export type AssetId = Tagged<'AssetId', UUIDv4>;
 class BasicFields {
   @Expose()
   @IsNotEmpty()
-  @IsIn(Object.values(AssetType))
-  type: AssetType;
-
-  @Expose()
-  @IsNotEmpty()
+  @IsString()
   @MinLength(2)
   @MaxLength(100)
   name: StringOfLength<2, 100>;
+
+  @Expose()
+  @IsOptional()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(500)
+  label?: StringOfLength<2, 500>;
+
+  @Expose()
+  @IsOptional()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(500)
+  description?: StringOfLength<2, 500>;
 }
 
-class CreateAssetDto {}
+class AdvancedFields extends BasicFields {
+  @Expose()
+  @IsNotEmpty()
+  @IsUUID()
+  organization: OrganizationId;
+
+  @Expose()
+  @IsNotEmpty()
+  @IsUrl()
+  url: Url;
+}
+
+export class CreateImageDto extends AdvancedFields {
+  @Expose()
+  @Expose()
+  @IsNotEmpty()
+  @IsIn(Object.values(AssetType))
+  type: AssetType.image;
+}
 
 export class Asset {}
 
-export class PrivateAsset extends BasicFields {
+export class PrivateAsset extends AdvancedFields {
   @Expose()
+  @IsNotEmpty()
   @IsUUID('4')
   public_id: AssetId;
 
-  organization: OrganizationId;
-  url: Url;
+  @Expose()
+  @IsNotEmpty()
+  @IsIn(Object.values(AssetType))
+  type: AssetType;
 
-  static create() {}
+  static createImage(
+    payload: unknown,
+    organization: OrganizationId,
+    fileUrl: Url,
+  ) {
+    return parseAndValidateUnknown(payload, BasicFields).pipe(
+      switchMap((parsed) => {
+        if (e.isLeft(parsed)) {
+          return toLeftObs(parsed.left);
+        }
+        return parseAndValidateUnknown(
+          {
+            ...parsed.right,
+            type: AssetType.image,
+            url: fileUrl,
+            organization,
+          },
+          CreateImageDto,
+        );
+      }),
+    );
+  }
 }
---
+++ libs/global/src/types/File.ts
@@ -0,0 +1,90 @@
+import { registerDecorator, ValidationOptions } from 'class-validator';
+import { NonEmptyArray } from './Array';
+import { Type } from './Class';
+
+import { Tagged } from './Tagged';
+
+export type CustomFile = Tagged<'__File__', File>;
+
+const ensureIsFile = (file: any) => {
+  return typeof file === 'object' && 'size' in file && 'name' in file;
+};
+
+const hasCorrectSize = (
+  file: CustomFile,
+  [maxSize, minSize]: [number | undefined, number | undefined],
+) => {
+  const max = maxSize || Infinity;
+  const min = minSize || 0;
+
+  return file.size < max && file.size > min;
+};
+
+const hasFileType = (file: CustomFile, types: NonEmptyArray<string>) => {
+  const fileType = file.name.split('.').pop();
+
+  return !!fileType && types.includes(fileType);
+};
+
+function IsFile(validationOptions?: ValidationOptions) {
+  return function (object: Type<Object>, propertyName: string) {
+    registerDecorator({
+      name: 'isCustomFile',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [propertyName],
+      options: validationOptions,
+      validator: {
+        validate(value: any) {
+          return ensureIsFile(value);
+        },
+      },
+    });
+  };
+}
+
+type MinMax = {
+  maxSize: number;
+  minSize: number;
+};
+
+function FileSize(validationOptions: ValidationOptions & Partial<MinMax>) {
+  return function (object: Type<Object>, propertyName: string) {
+    registerDecorator({
+      name: 'fileSize',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [propertyName],
+      options: validationOptions,
+      validator: {
+        validate(value: CustomFile) {
+          return hasCorrectSize(value, [
+            validationOptions.minSize,
+            validationOptions.maxSize,
+          ]);
+        },
+      },
+    });
+  };
+}
+
+function FileType(
+  validationOptions: ValidationOptions & { types: NonEmptyArray<string> },
+) {
+  return function (object: Type<Object>, propertyName: string) {
+    registerDecorator({
+      name: 'fileType',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [propertyName],
+      options: validationOptions,
+      validator: {
+        validate(value: CustomFile) {
+          return hasFileType(value, validationOptions.types);
+        },
+      },
+    });
+  };
+}
+
+export const CustomFile = { IsFile, FileSize, FileType };
