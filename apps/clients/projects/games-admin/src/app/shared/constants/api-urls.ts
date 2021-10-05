ba39bb17d8b42facf41949579250f428fbd3f076
Zdravko <zdravkokirilov@yahoo.com>
2021-10-05 09:00:37 +0300

    test admin deployment

--- apps/clients/projects/games-admin/src/app/shared/constants/api-urls.ts
+++ apps/clients/projects/games-admin/src/app/shared/constants/api-urls.ts
@@ -2,22 +2,20 @@ import { environment } from '@env';
 
 import { ApiUrls, buildApiUrls, Url } from '@end/global';
 
+if (environment.production && !process.env.API_HOST) {
+  throw new Error('API_HOST is a required env variable');
+}
+
+const apiHost = process.env.API_HOST || environment.apiHost;
+
 export const endpoints = {
-  requestAuthCode: Url.generate(
-    `${environment.apiHost}${ApiUrls.getLoginCode}`,
-  ),
-  requestAuthToken: Url.generate(
-    `${environment.apiHost}${ApiUrls.getAuthToken}`,
-  ),
-  getCurrentUser: Url.generate(
-    `${environment.apiHost}${ApiUrls.getCurrentUser}`,
-  ),
-  refreshAuthToken: Url.generate(
-    `${environment.apiHost}${ApiUrls.refreshAuthToken}`,
-  ),
-  logout: Url.generate(`${environment.apiHost}${ApiUrls.logout}`),
-  organization: Url.generate(`${environment.apiHost}/organization`),
+  requestAuthCode: Url.generate(`${apiHost}${ApiUrls.getLoginCode}`),
+  requestAuthToken: Url.generate(`${apiHost}${ApiUrls.getAuthToken}`),
+  getCurrentUser: Url.generate(`${apiHost}${ApiUrls.getCurrentUser}`),
+  refreshAuthToken: Url.generate(`${apiHost}${ApiUrls.refreshAuthToken}`),
+  logout: Url.generate(`${apiHost}${ApiUrls.logout}`),
+  organization: Url.generate(`${apiHost}/organization`),
 
   collection: (...args: Parameters<typeof buildApiUrls.collections>) =>
-    Url.generate(environment.apiHost + buildApiUrls.collections(...args)),
+    Url.generate(apiHost + buildApiUrls.collections(...args)),
 };
